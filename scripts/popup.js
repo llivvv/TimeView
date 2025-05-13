cee7465fdc1c0f1d9d5d5d68483097a3f45efe40
llivvv <livialin2014@gmail.com>
2025-05-12 18:54:09 -0700

    Revert "Create README.md"
    This reverts commit 2407e6b8d8884a7ff22293a52f5093839cd54a80.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,39 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
-  let ogDuration = message;
-  makeDurationList();
-  sendResponse({ message: "Duration received" });
-});
-
-makeDurationList() {
-ogMinSec = ogDuration.split(":");
-console.log(ogMinSec);
+let ogDuration;
+let ogDurInSec;
 
 const durList = document.querySelector(".dur-list");
 const speedList = document.querySelectorAll(".speed-elem");
 
-console.log("yay");
+// getting the duration of the original video
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message);
+  sendResponse({ message: "Duration received" });
+  console.log(typeof message);
+  ogDuration = JSON.stringify(message);
+  makeDurationList();
+});
 
+// making new duration
+function makeDurationList() {
+  ogMinSec = ogDuration.split(":").slice(1);
+  console.log(ogMinSec);
+
+  const ogMin = parseInt(ogMinSec[0]);
+  console.log(ogMin);
+  const ogSec = parseInt(ogMinSec[1]);
+  console.log(ogSec);
+  calcDur(ogMin, ogSec);
+  calcNewDur();
+
+  console.log("yay");
+}
+
+// original duration in seconds
+function calcDur(ogMin, ogSec) {
+  ogDurInSec = ogMin * 60 + ogSec;
+  console.log(ogDurInSec);
+}
+
+function calcNewDur() {
+  // speedList.forEach((speed) => {});
 }
