40827552df63b56c8693dbe7fc6e6f615a54d838
llivvv <livialin2014@gmail.com>
2025-05-12 19:21:16 -0700

    receive message correctly

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,17 +5,18 @@ const durList = document.querySelector(".dur-list");
 const speedList = document.querySelectorAll(".speed-elem");
 
 // getting the duration of the original video
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request.message);
   sendResponse({ message: "Duration received" });
-  console.log(typeof message);
-  ogDuration = JSON.stringify(message);
+  ogDuration = request.message;
+  // console.log(typeof message);
+  // ogDuration = JSON.stringify(message);
   makeDurationList();
 });
 
 // making new duration
 function makeDurationList() {
-  ogMinSec = ogDuration.split(":").slice(1);
+  ogMinSec = ogDuration.split(":");
   console.log(ogMinSec);
 
   const ogMin = parseInt(ogMinSec[0]);
