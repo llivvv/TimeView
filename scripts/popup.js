d2d831242d26aab080bb403d205783113f6aa462
llivvv <livialin2014@gmail.com>
2025-05-12 18:40:26 -0700

    started popup functions

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log(message);
   let ogDuration = message;
+  makeDurationList();
   sendResponse({ message: "Duration received" });
 });
 
+makeDurationList() {
 ogMinSec = ogDuration.split(":");
 console.log(ogMinSec);
 
@@ -11,3 +13,5 @@ const durList = document.querySelector(".dur-list");
 const speedList = document.querySelectorAll(".speed-elem");
 
 console.log("yay");
+
+}
