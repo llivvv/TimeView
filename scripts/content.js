6ef3f0ef18086f84a9a13d362ed81d063261bafe
llivvv <livialin2014@gmail.com>
2025-05-15 11:54:27 -0700

    clean up code + fix ad duration bug

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,8 @@
-const durElem = document.querySelector(".ytp-time-duration");
-const ogDur = durElem.innerHTML;
+let isDurAdded = false;
+let ogDur = 0;
+console.log(ogDur);
 
 const durList = [];
-let ogDurInSec = 0;
-let isDurAdded = false;
 
 function getAllClasses() {
   // get all the tags
@@ -23,52 +22,51 @@ function getAllClasses() {
 
 // getAllClasses();
 
-function convertOgDurToSec() {
-  let ogDurArr = ogDur.split(":");
-  let min = parseInt(ogDurArr[0]);
-  let sec = parseInt(ogDurArr[1]);
-  ogDurInSec = min * 60 + sec;
-}
-
 function calcDur(speed) {
   let speedText = speed.innerText;
   if (speedText === "Normal") {
-    let ogDurArr = ogDur.split(":");
-    let minute = parseInt(ogDurArr[0]);
-    let seconds = parseInt(ogDurArr[1]);
-    return { minute, seconds };
-  } else {
-    let speedNum = parseFloat(speedText);
-    let calc = ogDurInSec / speedNum;
-    let minute = Math.floor(calc / 60);
-    let seconds = Math.round(((calc % 60) * 100) / 100);
-    return { minute, seconds };
+    speedText = "1";
   }
+  let speedNum = parseFloat(speedText);
+  let calc = ogDur / speedNum;
+  let hours = Math.floor(calc / 3600);
+  calc %= 3600;
+  let minutes = Math.floor(calc / 60);
+  let seconds = Math.floor(calc % 60);
+  return { hours, minutes, seconds };
 }
 
-function displayDur(minSecObj, speed) {
+function displayDur(hoursMinSecObj, speed) {
   let newSpan = document.createElement("div");
-  newSpan.classList.add("ytp-menuitem-label");
   speed.appendChild(newSpan);
-  let paddedSec = String(minSecObj.seconds).padStart(2, "0");
-  newSpan.innerText = `${minSecObj.minute}:${paddedSec}`;
-  durList.push(`${minSecObj.minute}:${paddedSec}`);
+  let paddedMin = String(hoursMinSecObj.minutes).padStart(2, "0");
+  let paddedSec = String(hoursMinSecObj.seconds).padStart(2, "0");
+  if (hoursMinSecObj.hours == 0) {
+    newSpan.innerText = `${hoursMinSecObj.minutes}:${paddedSec}`;
+  } else {
+    newSpan.innerText = `${hoursMinSecObj.hours}:${paddedMin}:${paddedSec}`;
+  }
+  newSpan.classList.add("ytp-menuitem-label");
+  newSpan.classList.add(".new-duration");
+  durList.push(hoursMinSecObj);
+  console.log(durList);
 }
 
 function handleSpeedItemClick() {
-  if (isDurAdded) return;
-
   // wait for the speeds panel to show up first
   setTimeout(() => {
     getAllClasses();
+    if (document.querySelector(".new-duration") != null) return;
     const speedsWithCustom = document.querySelectorAll(".ytp-menuitem");
+    const custom = document.querySelector(".ytp-menuitem");
     console.log(speedsWithCustom);
     const speeds = Array.from(speedsWithCustom).slice(1);
-    convertOgDurToSec();
+    custom.style.width = "100%";
 
     speeds.forEach((speed) => {
-      const minSecObj = calcDur(speed);
-      displayDur(minSecObj, speed);
+      console.log(`Here is the parent: ${speed.closest("div").classList}`);
+      const hoursMinSecObj = calcDur(speed);
+      displayDur(hoursMinSecObj, speed);
     });
   }, 500);
 
@@ -76,8 +74,23 @@ function handleSpeedItemClick() {
 }
 
 function getMenuItems() {
-  const child = document.querySelector(".ytp-panel-menu").children[5];
-  child.addEventListener("click", handleSpeedItemClick);
+  console.log("reached settings btn");
+  const allChildren = document.querySelector(".ytp-panel-menu").children;
+  console.log(allChildren.length);
+  let child = null;
+
+  // speed item doesn't have a fixed position in list
+  for (let i = 0; i < allChildren.length; i++) {
+    if (allChildren[i].children[1].innerText == "Playback speed") {
+      ogDur = document.querySelector("video").duration;
+      child = allChildren[i].children[1];
+      break;
+    }
+  }
+
+  if (child != null) {
+    child.addEventListener("click", handleSpeedItemClick);
+  }
 }
 
 const settingsBtn = document.querySelector(".ytp-settings-button");
