6b84c7928193e1eb0e0a9563f84b255d2e682bcb
llivvv <livialin2014@gmail.com>
2025-05-15 21:47:42 -0700

    display duration when speedslider changes value

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,12 @@
 let isDurAdded = false;
 let ogDur = 0;
+let speedsWithCustom = null;
+let speedSlider = null;
+let speeds = null;
+
 console.log(ogDur);
 
 const durList = [];
-
-function getAllClasses() {
-  // get all the tags
-  const allTagsArray = [...document.getElementsByTagName("*")];
-
-  // get a flattened list of each tag's classList
-  const allClasses = allTagsArray.map((tag) => [...tag.classList]).flat();
-
-  // get a Set of those classes to remove duplicates
-  const uniqueClasses = new Set(allClasses);
-
-  // sort alphabetically and join all the classes by newline for display/readability
-  const allUniqueClassesJoinedByNewline = [...uniqueClasses].sort().join("\n");
-
-  console.log(allUniqueClassesJoinedByNewline);
-}
-
-// getAllClasses();
 
 function calcDur(speed) {
   let speedText = speed.innerText;
@@ -37,56 +23,68 @@ function calcDur(speed) {
 }
 
 function displayDur(hoursMinSecObj, speed) {
-  let newSpan = document.createElement("div");
-  speed.appendChild(newSpan);
   let paddedMin = String(hoursMinSecObj.minutes).padStart(2, "0");
   let paddedSec = String(hoursMinSecObj.seconds).padStart(2, "0");
-  if (hoursMinSecObj.hours == 0) {
-    newSpan.innerText = `${hoursMinSecObj.minutes}:${paddedSec}`;
-  } else {
-    newSpan.innerText = `${hoursMinSecObj.hours}:${paddedMin}:${paddedSec}`;
-  }
-  newSpan.classList.add("ytp-menuitem-label");
-  newSpan.classList.add(".new-duration");
+
+  speed.children[0].innerText +=
+    hoursMinSecObj.hours == 0
+      ? ` (${hoursMinSecObj.minutes}:${paddedSec})`
+      : ` (${hoursMinSecObj.hours}:${paddedMin}:${paddedSec})`;
+  speed.classList.add("new-duration");
   durList.push(hoursMinSecObj);
   console.log(durList);
+}
+
+function calcAndDisplayDur(speeds) {
+  if (document.querySelector(".new-duration") != null) return;
+
+  speeds.forEach((speed) => {
+    const hoursMinSecObj = calcDur(speed);
+    displayDur(hoursMinSecObj, speed);
+  });
+}
+
+function selectSpeedElems() {
+  speedsWithCustom = document.querySelectorAll(".ytp-menuitem");
+  speedSlider = document.querySelector(".ytp-speedslider");
+  speeds = Array.from(speedsWithCustom).slice(1);
+  console.log(speeds);
 }
 
 function handleSpeedItemClick() {
   // wait for the speeds panel to show up first
   setTimeout(() => {
-    getAllClasses();
-    if (document.querySelector(".new-duration") != null) return;
-    const speedsWithCustom = document.querySelectorAll(".ytp-menuitem");
-    const custom = document.querySelector(".ytp-menuitem");
-    console.log(speedsWithCustom);
-    const speeds = Array.from(speedsWithCustom).slice(1);
-    custom.style.width = "100%";
-
-    speeds.forEach((speed) => {
-      console.log(`Here is the parent: ${speed.closest("div").classList}`);
-      const hoursMinSecObj = calcDur(speed);
-      displayDur(hoursMinSecObj, speed);
+    selectSpeedElems();
+    speedSlider.addEventListener("change", () => {
+      setTimeout(() => {
+        // set elements again because new elements are rendered on screen
+        selectSpeedElems();
+        calcAndDisplayDur(speeds);
+      }, 300);
     });
-  }, 500);
 
-  isDurAdded = true;
+    calcAndDisplayDur(speeds);
+  }, 500);
+}
+
+function findSpeedItem(allChildren) {
+  // speed item doesn't have a fixed position in list
+  for (let i = 0; i < allChildren.length; i++) {
+    if (allChildren[i].children[1].innerText == "Playback speed") {
+      ogDur = document.querySelector("video").duration;
+      return allChildren[i].children[1];
+    }
+  }
+  return null;
 }
 
 function getMenuItems() {
   console.log("reached settings btn");
   const allChildren = document.querySelector(".ytp-panel-menu").children;
   console.log(allChildren.length);
-  let child = null;
+  // let child = null;
 
-  // speed item doesn't have a fixed position in list
-  for (let i = 0; i < allChildren.length; i++) {
-    if (allChildren[i].children[1].innerText == "Playback speed") {
-      ogDur = document.querySelector("video").duration;
-      child = allChildren[i].children[1];
-      break;
-    }
-  }
+  let child = findSpeedItem(allChildren);
 
   if (child != null) {
     child.addEventListener("click", handleSpeedItemClick);
